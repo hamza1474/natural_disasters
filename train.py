b99c913487a5731be2aff3ae1c7593289e2292a0
hamza1474 <ameerhamza1474@gmail.com>
2019-11-20 05:28:10 +0500

    train.py complete

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ import pickle
 import cv2
 import sys
 import os
-#argparsing
+# argparsing
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--lr-find", type=int, default=0,
 	help="whether or not to find optimal learning rate")
@@ -30,8 +30,8 @@ print("[INFO] loading images...")
 imagePaths = list(paths.list_images(config.DATASET_PATH))
 data = []
 labels = []
-#get data and convert to arrays and onehot
-for imagePaths in imagePaths:
+# get data and convert to arrays and onehot
+for imagePath in imagePaths:
 	label = imagePaths.split(os.path.sep)[-2]
 	image = cv2.imread(imagePath)
 	image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -43,8 +43,108 @@ for imagePaths in imagePaths:
 print("[INFO] processing data...")
 data = np.array(data, dtype="float32")
 labels = np.array(labels)
-#onehot
+# Onehot
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
 
+# Train Test Val split
+(trainX, testX, trainY, testY) = train_test_split(data, labels, 
+	test_size=config.TEST_SPLIT, random_state=42)
+trainX, valX, trainY, valY) = train_test_split(trainX, trainY, 
+	test_size=config.VAL_SPLIT, random_state=84)
 
+# data augmentation
+aug = ImageDataGenerator(
+	rotation_range=30,
+	zoom_range=0.15,
+	width_shift_range=0.2,
+	height_shift_range=0.2,
+	shear_range=0.15,
+	horizontal_flip=True,
+	fill_mode="nearest")
+
+# Model
+base_model = VGG16(include_top=False, weights="imagenet",
+	input_tensor=Input(shape=(224, 224, 3)), trainable=False)
+
+X = base_model.output
+X = Flatten(name="flatten")(X)
+X = Dense(512, activation="relu")(X)
+X = Dropout(0.5)(X)
+X = Dense(len(config.CLASSES), activation="softmax")(X)
+
+model = Model(inputs=base_model.input, outputs=X)
+
+print("[INFO] compiling model...")
+opt = SGD(lr=config.MIN_LR, momentum=0.9)
+model.compile(loss="categorical_crossentropy", optimizer=opt,
+	metrics=["accuracy"])
+
+# Learning Rate Finder
+if args["lr_find"] > 0:
+	print("[INFO] findinf learning rate...")
+	lrf = LearningRateFinder(model)
+	lrf.find(
+		aug.flow(trainX, trainY, batch_size=config.BATCH_SIZE),
+		1e-10, 1e+1,
+		stepsPerEpoch=np.ceil((trainX.shape[0]/float(config.BATCH_SIZE))),
+		epochs=20
+		batchSize=config.BATCH_SIZE)
+	
+	lrf.plot_loss()
+	plt.savefig(config.LRFIND_PLOT_PATH)
+
+	print("[INFO] learning rate finder complete...")
+	print("[INFO] examine plot and adjust learning rate before training")
+	sys.exit(0)
+
+# Continue after adjusting Learning Rates
+stepSize = config.STEP_SIZE * (trainX.shape[0] // config.BATCH_SIZE)
+clr = CyclicLR(
+	mode=config.CLR_METHOD,
+	base_lr=config.MIN_LR,
+	max_lr=config.MAX_LR,
+	step_size=stepSize
+)
+
+# train
+print("[INFO] training...")
+history = model.fit_generator(
+	aug.flow(trainX, trainY, batchSize=config.BATCH_SIZE),
+	validation_data=(valX, valY),
+	steps_per_epoch=trainX.shape[0]//config.BATCH_SIZE,
+	epochs=config.NUM_EPOCHS,
+	callbacks=[clr],
+	verbose=1)
+
+# Evaluate
+print("[INFO] evaluating network...")
+predictions = model.predict(testX, batch_size=config.BATCH_SIZE)
+print(classification_report(testY.argmax(axis=1),
+	predictions.argmax(axis=1), target_names=config.CLASSES))
+
+print("[INFO] serializing network to '{}'...".format(config.MODEL_PATH))
+model.save(config.MODEL_PATH)
+
+# plot
+
+N = np.arange(0, config.NUM_EPOCHS)
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(N, history.history["loss"], label="train_loss")
+plt.plot(N, history.history["val_loss"], label="val_loss")
+plt.plot(N, history.history["accuracy"], label="train_acc")
+plt.plot(N, history.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epochs")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig(config.TRAINING_PLOT_PATH)
+
+N = np.arange(0, len(clr.history["lr"]))
+plt.figure()
+plt.plot(N, clr.history["lr"])
+plt.title("Cyclical Learning Rate (CLR)")
+plt.xlabel("Training Iterations")
+plt.ylabel("Learning Rate")
+plt.savefig(config.CLR_PLOT_PATH)
